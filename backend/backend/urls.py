fecc03e7293c08e35bf308e16a2e316761e325f4
Harsha Pathuri <harsha.pathuri@students.iiit.ac.in>
2023-05-12 19:30:04 +0530

    upd: only api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 # from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('sales/', include('sales.urls')),
+    # path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('', TemplateView.as_view(template_name = 'home.html'), name='home'),
 ]
 
 # if settings.DEBUG:
--- backend/sales/urls.py
+++ backend/sales/urls.py
@@ -2,21 +2,9 @@ from rest_framework import routers
 from django.urls import path, include
 from . import views
 
-# router = routers.DefaultRouter()
-# router.register(r'items', views.ItemViewSet)
-# router.register(r'item-prices', views.ItemPriceViewSet)
-# router.register(r'daily-sales', views.DailySaleViewSet)
-# router.register(r'food-sales', views.FoodSaleViewSet)
-
-# urlpatterns = router.urls
-
-urlpatterns = [
-    path(r'items/', views.ItemList.as_view()),
-    path(r'items/<int:itemId>', views.ItemDetail.as_view()),
-    path(r'item-prices/', views.ItemPriceList.as_view()),
-    path(r'item-prices/<int:itemPriceId>', views.ItemPriceDetail.as_view()),
-    path(r'daily-sales/', views.DailySaleList.as_view()),
-    path(r'daily-sales/<int:dailySaleId>', views.DailySaleDetail.as_view()),
-    path(r'food-sales/', views.FoodSaleList.as_view()),
-    path(r'food-sales/<int:foodSaleId>', views.FoodSaleDetail.as_view()),
-]
+router = routers.DefaultRouter()
+router.register(r'items', views.ItemViewSet)
+router.register(r'item-prices', views.ItemPriceViewSet)
+router.register(r'daily-sales', views.DailySaleViewSet)
+router.register(r'food-sales', views.FoodSaleViewSet)
+urlpatterns = router.urls
